4c874def86a89b84e47d76197546eb4e504a81ae
Leonardo <leomicheloni@hotmail.com>
2015-08-18 22:30:19 -0300

    Initial structure

---
+++ autocomplete.js
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var Autocomplete = React.createClass({
+	render: function(){
+		return (<div>hola</div>);
+	}
+})
+
+module.exports = Autocomplete;
---
+++ main.js
@@ -0,0 +1,5 @@
+var React = require("react");
+var Autocomplete = require("./autocomplete");
+
+
+React.render(<Autocomplete />, document.getElementById("container"));
