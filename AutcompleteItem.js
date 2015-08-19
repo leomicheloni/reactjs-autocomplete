039ac8adbb206b0c6317964967e5d244a58802a1
Leonardo <leomicheloni@hotmail.com>
2015-08-19 14:03:25 -0300

    added list item component

---
+++ AutcompleteItem.js
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var AutocompleteItem = React.createClass({
+	render: function(){
+		return (<li>item</li>));
+	}
+})
+
+module.exports = AutocompleteItem;
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var React = require("react");
 var Autocomplete = require("./autocomplete");
 
+var items = [
+{"desc": "Ariel Ortega", "id": 1},
+{"desc": "Hernan Crespo", "id": 2},
+{"desc": "Rodrigo Mora", "id": 3},
+{"desc": "Matías Kraneviter", "id": 4},
+{"desc": "Leonardo Ponzio", "id": 5},
+{"desc": "Marcelo Barovero", "id": 6}
+];
 
-React.render(<Autocomplete />, document.getElementById("container"));
+React.render(<Autocomplete data={items} />, document.getElementById("container"));
