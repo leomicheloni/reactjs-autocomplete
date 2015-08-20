f4d2ba7e63552c632cd8a27f4d268c14f9b16220
Leonardo <leomicheloni@hotmail.com>
2015-08-20 19:03:54 -0300

    connecting components, now its a list

--- AutcompleteItem.js
+++ AutcompleteItem.js
@@ -2,7 +2,7 @@ var React = require("react");
 
 var AutocompleteItem = React.createClass({
 	render: function(){
-		return (<li>item</li>));
+		return (<li>{this.props.value}</li>);
 	}
 })
 
--- autocomplete.js
+++ autocomplete.js
@@ -1,8 +1,15 @@
 var React = require("react");
+var AutocompleteItem= require("./AutcompleteItem");
 
 var Autocomplete = React.createClass({
 	render: function(){
-		return (<div>hola</div>);
+		return (
+			<ul>
+			{this.props.data.map(function(item){
+					return (<AutocompleteItem value={item.desc} />)
+			})}
+			</ul>
+		);
 	}
 })
 
